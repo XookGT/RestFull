1311c6f28c6210b7c1431337a6784cd93d12e16a
Dayton Garcia <dayton.garcia@simbitgt.com>
2017-05-12 14:23:50 -0600

    fix

--- app/Http/Middleware/TokenEntrustAbility.php
+++ app/Http/Middleware/TokenEntrustAbility.php
@@ -16,7 +16,7 @@ class TokenEntrustAbility extends BaseMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next, $roles, $permissions, $validateAll = false)
+    /*public function handle($request, Closure $next, $roles, $permissions, $validateAll = false)
     {
         if (! $token = $this->auth->setRequest($request)->getToken()) {
             return $this->respond('tymon.jwt.absent', 'token_not_provided :(', 400);
@@ -38,6 +38,29 @@ class TokenEntrustAbility extends BaseMiddleware
             return $this->respond('tymon.jwt.invalid', 'token_invalid', 401, 'Unauthorized');
         }
         
+        $this->events->fire('tymon.jwt.valid', $user);
+
+        return $next($request);
+    }*/
+
+        public function handle($request, \Closure $next)
+    {
+        if (! $token = $this->auth->setRequest($request)->getToken()) {
+            return $this->respond('tymon.jwt.absent', 'token_not_provided :)', 400);
+        }
+
+        try {
+            $user = $this->auth->authenticate($token);
+        } catch (TokenExpiredException $e) {
+            return $this->respond('tymon.jwt.expired', 'token_expired', $e->getStatusCode(), [$e]);
+        } catch (JWTException $e) {
+            return $this->respond('tymon.jwt.invalid', 'token_invalid', $e->getStatusCode(), [$e]);
+        }
+
+        if (! $user) {
+            return $this->respond('tymon.jwt.user_not_found', 'user_not_found', 404);
+        }
+
         $this->events->fire('tymon.jwt.valid', $user);
 
         return $next($request);
