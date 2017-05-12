5f411370b18ec645b286789ef29d810aacbcff66
Dayton Garcia <dayton.garcia@simbitgt.com>
2017-05-12 10:36:45 -0600

    test

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'apis', 'middleware' => ['ability:admin,create-users']], function()
+{
+    // Protected route
+    Route::get('users', 'JwtAuthenticateController@index');
+});
+
