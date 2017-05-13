084a348e6ef418e256dc146fe22eda3362da08ab
Dayton Garcia <dayton.garcia@simbitgt.com>
2017-05-12 23:57:17 -0600

    fixed Courses controller

--- app/Http/Controllers/Courses.php
+++ app/Http/Controllers/Courses.php
@@ -54,7 +54,6 @@ class Courses extends Controller
             $course->id_categorie = $request->id_categorie;
             $course->id_level = $request->id_level;
             $course->starts = 0;
-            $course->rank = 0;
 
             
             $course->save();
